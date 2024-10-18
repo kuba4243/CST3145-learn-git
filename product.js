0889a4eee492b1ae8ed9a9960d01d691361cfbdf
Kuba sobczak <Ks1648@live.mdx.ac.uk>
2024-10-18 15:59:51 +0100

    created product.js file wich consits of the lessos to fill the product array

---
+++ product.js
@@ -0,0 +1,82 @@
+let products = [
+    {
+        id: 1001,
+        subject: "Mathematics",
+        location: "London",
+        price: 49.99,
+        image: "images\\math.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1002,
+        subject: "English Language",
+        location: "Manchester",
+        price: 54.99,
+        image: "images\\ENglish Lang.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1003,
+        subject: "English Literature",
+        location: "Leeds",
+        price: 59.99,
+        image: "images\\ENGLIT.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1004,
+        subject: "Science",
+        location: "Birmingham",
+        price: 19.99,
+        image: "images\\Sciecne.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1005,
+        subject: "P.E",
+        location: "Liverpool",
+        price: 24.99,
+        image: "images\\PE.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1006,
+        subject: "RE",
+        location: "Glasgow",
+        price: 9.99,
+        image: "images\\RE.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1007,
+        subject: "ICT",
+        location: "Edinburgh",
+        price: 64.99,
+        image: "images\\ICt.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1008,
+        subject: "Drama",
+        location: "Cardiff",
+        price: 21.99,
+        image: "images\\Drama.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1009,
+        subject: "Music",
+        location: "Newcastle",
+        price: 24.99,
+        image: "images\\music.webp",
+        availableSpace: 5
+    },
+    {
+        id: 1010,
+        subject: "History",
+        location: "Sheffield",
+        price: 17.99,
+        image: "images\\history.webp",
+        availableSpace: 5
+    }
+];
